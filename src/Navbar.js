a9ed8635d453a2a0bfda679cee65150d1c7ca3fd
Jordi <jordicastellon@hotmail.com>
2021-06-13 19:31:31 +0200

    starting files

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+            <nav ClassName="navbar">
+                <h1>The Dojo Blog</h1>
+                <div className="links">
+                    <a href="/">Home</a>
+                    <a href="/create">New Blog</a>
+                </div>
+            </nav>
+
+      );
+}
+ 
+export default Navbar;
