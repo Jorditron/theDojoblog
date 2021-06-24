6d22afee42162f55bc42fb637589634fc7b4e025
Jordi <jordicastellon@hotmail.com>
2021-06-24 22:43:34 +0200

    video16

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList =({blogs,title})=>{
+const BlogList =({blogs,title,handleDelete})=>{
 
   //  const blogs=props.blogs;
   //  const title=props.title;
@@ -6,10 +6,13 @@ const BlogList =({blogs,title})=>{
 
         <div className="blog-list">
             <h2>{title}</h2>
+            
         {blogs.map((blog) =>(
         <div className="blog-preview" key={blog.id}>
             <h2>{blog.title}</h2>
+            
             <p>Written by {blog.author }</p>
+            <button onClick={()=>handleDelete(blog.id)}>Delete blog</button>
         </div>
 
         ))}
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,30 @@
-  
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
-const Home= () => {
-    const [blogs,setBlogs]= useState([
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+const [name, setName]=useState("mario");
 
-        {title:"My new website", body:"lorem ipsum...",author:"mario", id:1},
-        {title:"Welcome party!", body:"lorem ipsum...",author:"yoshi", id:2},
-        {title:"Web dev top tips", body:"lorem ipsum...",author:"mario", id:3},
-        
-    ]);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
-    return ( 
-    
-        <div className="home">
-     
-        <BlogList blogs={blogs} title="All blogs!"/>
-        <BlogList blogs={blogs.filter((blog)=>blog.author ==="mario")} title="Mario's blogs"/>
-        </div>
-        
-       
-     );
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(name);
+  },[name]);
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+        <button onClick={()=>setName("luigi")}>Change name</button>
+    </div>
+  );
 }
  
 export default Home;
-
